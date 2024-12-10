54a4591363798be2e14c2d65b3517ebaa277d408
daniellpan8 <daniellpan8@gmail.com>
2024-12-10 16:28:10 -0500

    add nav

---
+++ loadNavbar.js
@@ -0,0 +1,32 @@
+function loadNavbar() {
+    const navbarHTML = `
+        <div class="topnav">
+            <h2 href="index.html" class="active">DaniellPan.com</h2>
+            <div id="myLinks">
+                <a href="index.html">Home</a>
+                <a href="tutorials.html">Tutorials</a>
+                <a href="projects.html">Projects</a>
+            </div>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                <i class="fa fa-bars"></i>
+            </a>
+        </div>
+    `;
+
+    document.getElementById('navbar-container').innerHTML = navbarHTML;
+
+    // Add the myFunction script
+    function myFunction() {
+        var x = document.getElementById("myLinks");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    }
+
+    // Attach the function to the window object
+    window.myFunction = myFunction;
+}
+
+document.addEventListener('DOMContentLoaded', loadNavbar);
